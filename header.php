6b0964643a793278c542bd42afe7c13a31feb48a
rudzki <chris@rudzki.org>
2016-05-24 22:25:45 -0400

    Use free work sans font

--- header.php
+++ header.php
@@ -8,15 +8,13 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 <?php wp_head(); ?>
-<script src="https://use.typekit.net/lan7oth.js"></script>
-<script>try{Typekit.load({ async: true });}catch(e){}</script>
 </head>
 
 <body <?php body_class(); ?>>
